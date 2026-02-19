b852dde5c5ff83f524b0418b67236c201e9cdbe7
vyFahde <txneto2005@gmail.com>
2026-02-19 20:21:44 -0300

    Criação construtores Customer

--- ecommerce/src/main/java/com/example/ecommerce/model/Customer.java
+++ ecommerce/src/main/java/com/example/ecommerce/model/Customer.java
@@ -7,6 +7,14 @@ import java.math.BigInteger;
 import java.text.Normalizer;
 import java.util.Date;
 
+/*
+Entity -> Com essa Class nós conseguimos criar as Entidades(Tablas do banco)
+@Id -> Id da tabela
+@GeneratedValue -> Auto increment do banco
+UUID  -> Criptografia do dado.
+*/
+
+
 @Entity
 public class Customer {
     //Dados de login
@@ -21,6 +29,7 @@ public class Customer {
     @Column (nullable = false)
     private String password;
 
+    // Dados pessoais
     @Column (nullable = false)
     private String name;
 
@@ -35,10 +44,64 @@ public class Customer {
 
     @Column
     private Normalizer.Form gender;
+    //Nesta sessão vamos criar o formulario.
 
     @Column
     private Normalizer.Form sex;
-
+    //Dados de Contato
     @Column (unique = true, nullable = false)
     private BigInteger contactNumber;
+
+
+    public Customer(){
+
+    }
+
+    public Customer(UUID id,
+                    String email,
+                    String password,
+                    String cep,
+                    BigInteger cpf,
+                    Date birth){
+
+                    this.id = id;
+                    this.email = email;
+                    this.password = password;
+                    this.cep = cep;
+                    this.cpf = cpf;
+                    this.birth = birth;
+
+    }
+    // 3️⃣ Cadastro sem dados opcionais (sexo/gênero)
+    public Customer(String email,
+                    String password,
+                    String name,
+                    String cep,
+                    BigInteger cpf,
+                    Date birth,
+                    BigInteger contactNumber) {
+
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.cep = cep;
+        this.cpf = cpf;
+        this.birth = birth;
+        this.contactNumber = contactNumber;
+    }
+
+    // 4️⃣ Uso interno / testes (com ID)
+    public Customer(UUID id,
+                    String email,
+                    String password,
+                    String name,
+                    String cep,
+                    BigInteger cpf,
+                    Date birth,
+                    BigInteger contactNumber) {
+
+        this(email, password, name, cep, cpf, birth, contactNumber);
+        this.id = id;
+    }
 }
+
